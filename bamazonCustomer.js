8d447217c7f2bd43d443c0a3d0a24d8bf3218c31
Thomas Mylott <mylottiv@gmail.com>
2019-10-19 06:29:19 -0500

    Inquirer prompts now wrapped inside functions for recursive calls depending upon whether user wants to make another purchase or not

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,28 +14,28 @@ connection.connect(err => (err) ? console.log('Error:',err) : console.log('Conne
 connection.query('SELECT item_id, product_name, price FROM products', function (err, products){
     if (err) throw err;
     console.table(products)
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'chooseProduct',
-            message: 'Choose a product to purchase',
-            choices: function() {
-                const output = products.map((product) => `${product.item_id}) ${product.product_name}`);
-                return output;
+    function buyProduct(){
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'chooseProduct',
+                message: 'Choose a product to purchase',
+                choices: function() {
+                    const output = products.map((product) => `${product.item_id}) ${product.product_name}`);
+                    return output;
+                },
+                filter: function(choice) {
+                    return parseInt(choice.charAt(0));
+                }
             },
-            filter: function(choice) {
-                return parseInt(choice.charAt(0));
+            {
+                type: 'number',
+                name: 'chooseQuantity',
+                message: 'How many do you want to buy?',
+                default: 1
             }
-        },
-        {
-            type: 'number',
-            name: 'chooseQuantity',
-            message: 'How many do you want to buy?'
-        }
-    ]).then(function(answers) {
-        connection.query('SELECT stock_quantity FROM products WHERE item_id = ?',
-            [answers.chooseProduct],
-            function (err, res){
+        ]).then(function(answers) {
+            connection.query('SELECT stock_quantity FROM products WHERE item_id = ?', [answers.chooseProduct], function (err, res){
                 if (err) throw err;
                 const currentQuantity = res[0].stock_quantity;
                 const purchaseQuantity = answers.chooseQuantity;
@@ -47,13 +47,32 @@ connection.query('SELECT item_id, product_name, price FROM products', function (
                         function(err) {
                             if (err) throw err;
                             console.log(`Buying ${purchaseQuantity} ${products[productID].product_name}s for ${productPrice} each for a total price of ${totalPrice}`);
+                            startOver();
                         }
                     );
                 }
                 else {
                     console.log('Not enough quantity!')
+                    startOver();
                 };
-                connection.end();
-            })
-    });
+                function startOver() {
+                    inquirer.prompt([
+                        {
+                            type: 'confirm',
+                            name: 'startOver',
+                            message: 'Purchase another product?'
+                        }
+                    ]).then(function(answer){
+                        if (answer.startOver === true) {
+                            buyProduct();
+                        }
+                        else {
+                            connection.end();
+                        };
+                    });
+                };
+            });
+        });
+    };
+    buyProduct();
 });
