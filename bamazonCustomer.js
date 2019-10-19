a440b766dc95cbc872f97deeb1aa06cd53c28e61
Thomas Mylott <mylottiv@gmail.com>
2019-10-19 06:03:40 -0500

    Takes in user input for product id and quantity, checks if enough of product is in stock, and if so adjusts DB product stock accordingly.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,8 +10,50 @@ const connection = mysql.createConnection({
 
 connection.connect(err => (err) ? console.log('Error:',err) : console.log('Connected successfully'));
 
-connection.query('SELECT item_id, product_name, price FROM products', function (error, results){
-    console.table(results);
-});
 
-connection.end();
+connection.query('SELECT item_id, product_name, price FROM products', function (err, products){
+    if (err) throw err;
+    console.table(products)
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'chooseProduct',
+            message: 'Choose a product to purchase',
+            choices: function() {
+                const output = products.map((product) => `${product.item_id}) ${product.product_name}`);
+                return output;
+            },
+            filter: function(choice) {
+                return parseInt(choice.charAt(0));
+            }
+        },
+        {
+            type: 'number',
+            name: 'chooseQuantity',
+            message: 'How many do you want to buy?'
+        }
+    ]).then(function(answers) {
+        connection.query('SELECT stock_quantity FROM products WHERE item_id = ?',
+            [answers.chooseProduct],
+            function (err, res){
+                if (err) throw err;
+                const currentQuantity = res[0].stock_quantity;
+                const purchaseQuantity = answers.chooseQuantity;
+                const productID = answers.chooseProduct - 1;
+                const productPrice = products[productID].price
+                const totalPrice = purchaseQuantity * productPrice
+                if (currentQuantity >= purchaseQuantity) {
+                    connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [currentQuantity - purchaseQuantity, productID + 1],
+                        function(err) {
+                            if (err) throw err;
+                            console.log(`Buying ${purchaseQuantity} ${products[productID].product_name}s for ${productPrice} each for a total price of ${totalPrice}`);
+                        }
+                    );
+                }
+                else {
+                    console.log('Not enough quantity!')
+                };
+                connection.end();
+            })
+    });
+});
