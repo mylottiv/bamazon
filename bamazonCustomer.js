2e73138d399d84b5774e77bbbc0657d34ce31834
Thomas Mylott <mylottiv@gmail.com>
2019-10-19 06:39:22 -0500

    Added proper comments to code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,9 @@
+// Initialize Modules
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+// Initialize MySQL DB connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -10,12 +13,16 @@ const connection = mysql.createConnection({
 
 connection.connect(err => (err) ? console.log('Error:',err) : console.log('Connected successfully'));
 
-
+// Query DB for list of all products to be printed to console as a table
 connection.query('SELECT item_id, product_name, price FROM products', function (err, products){
+
     if (err) throw err;
     console.table(products)
+    
+    // Main user Input loop
     function buyProduct(){
         inquirer.prompt([
+            // Asks user to choose a product from the list returned from DB
             {
                 type: 'list',
                 name: 'chooseProduct',
@@ -28,6 +35,7 @@ connection.query('SELECT item_id, product_name, price FROM products', function (
                     return parseInt(choice.charAt(0));
                 }
             },
+            // Asks user for the quantity of product to be purchased
             {
                 type: 'number',
                 name: 'chooseQuantity',
@@ -35,17 +43,25 @@ connection.query('SELECT item_id, product_name, price FROM products', function (
                 default: 1
             }
         ]).then(function(answers) {
+            // Selects stock for chosen product from SQL DB
             connection.query('SELECT stock_quantity FROM products WHERE item_id = ?', [answers.chooseProduct], function (err, res){
+                
                 if (err) throw err;
+
+                // Assigns returned product information into semantically relevant variables
                 const currentQuantity = res[0].stock_quantity;
                 const purchaseQuantity = answers.chooseQuantity;
                 const productID = answers.chooseProduct - 1;
                 const productPrice = products[productID].price
                 const totalPrice = purchaseQuantity * productPrice
+
+                // Checks if there is enough stock_quantity to fulfill the user's purchase
                 if (currentQuantity >= purchaseQuantity) {
+                    // Updates the product entry in DB with the post-purchase quantity
                     connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [currentQuantity - purchaseQuantity, productID + 1],
                         function(err) {
                             if (err) throw err;
+                            // Outputs summmary of user order to console
                             console.log(`Buying ${purchaseQuantity} ${products[productID].product_name}s for ${productPrice} each for a total price of ${totalPrice}`);
                             startOver();
                         }
@@ -55,6 +71,7 @@ connection.query('SELECT item_id, product_name, price FROM products', function (
                     console.log('Not enough quantity!')
                     startOver();
                 };
+                // Check called at end of main input loop to see if user wishes to exit or make another purchase
                 function startOver() {
                     inquirer.prompt([
                         {
