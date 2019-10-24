2a2388ec38c4d6a3a32fe67f5bc86bd075ef0006
Thomas Mylott <mylottiv@gmail.com>
2019-10-24 12:59:47 -0500

    At expense of error handling, mysql query promise now handled correctly when called for addStock question

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,104 +1,109 @@
 // Initialize Modules
 const cTable = require('console.table');
 const inquirer = require('inquirer');
-const mysql = require('mysql');
+// const mysql = require('mysql');
+const mysqlDB = require('mysql-promise')();
 
 // Initialize MySQL DB connection
-const connection = mysql.createConnection({
+mysqlDB.configure({
     host: 'localhost',
     user: 'root',
     password: 'porpoise',
     database: 'bamazon_db'
 });
 
-connection.connect(err => (err) ? console.log('Error:',err) : console.log('Connected successfully'));
-
+// connection.connect(err => (err) ? console.log('Error:',err) : console.log('\nConnected successfully'));
+managerInput();
 function managerInput(){
-    connection.query('SELECT item_id, product_name, price, stock_quantity FROM products', function (err, products){
-        if (err) throw err;
-        inquirer.prompt([
-            // Main input question
-            {
-                type: 'list',
-                name: 'chooseAction',
-                message: 'Choose an action',
-                choices: ['View all products', 'View low inventory', 'Add to inventory', 'Add new product']
+    inquirer.prompt([
+        // Main input question
+        {
+            type: 'list',
+            name: 'chooseAction',
+            message: 'Choose an action',
+            choices: ['View all products', 'View low inventory', 'Add to inventory', 'Add new product']
+        },
+        // If adding to inventory prompt user to select a product
+        {
+            type: 'list',
+            name: 'chooseProduct',
+            message: 'Choose a product',
+            when: function(answers) {
+                return answers.chooseAction === 'Add to inventory'
             },
-            // If adding to inventory prompt user to select a product
-            {
-                type: 'list',
-                name: 'chooseProduct',
-                message: 'Choose a product',
-                when: function(answers) {
-                    return answers.chooseAction === 'Add to inventory'
-                },
-                choices: function() {
-                    const output = products.map(product => `${product.item_id}) ${product.product_name} Stock: ${product.stock_quantity}`);
-                    return output
-                },
-                filter: choice => parseInt(choice.charAt(0))
+            choices: () => populateInventory('addStock'),
+            filter: choice => parseInt(choice.charAt(0))
+        },
+        // If adding to inventory after inputting product prompt for quantity
+        {
+            type: 'number',
+            name: 'addStock',
+            message: 'How much to restock?',
+            when: function(answers) {
+                return answers.chooseProduct !== undefined;
             },
-            // If adding to inventory after inputting product prompt for quantity
-            {
-                type: 'number',
-                name: 'addStock',
-                message: 'How much to restock?',
-                when: function(answers) {
-                    return answers.chooseProduct !== undefined;
-                },
-                default: 1,
-                validate: function(answer){
-                    return (Number.isNaN(answer)) ? 'Please enter a valid number' : true;
-                }
-            },
-            // Prompt user for new product name if Add new product chosen
-            {
-                type: 'input',
-                name: 'newProductName',
-                message: 'Name of product to add?',
-                when: function(answers) {
-                    return answers.chooseAction === 'Add new product';
-                },
-            },
-            // Prompt user for new product department if relevant
-            {
-                type: 'input',
-                name: 'newProductDepartment',
-                message: 'Department of product to add?',
-                when: function(answers) {
-                    return answers.newProductName !== undefined;
-                }
-            },
-            // Prompt user for new product price if relevant
-            {
-                type: 'number',
-                name: 'newProductPrice',
-                message: 'Price of product to add?',
-                when: function(answers){
-                    return answers.newProductDepartment !== undefined;
-                },
-                default: 1.00,
-                validate: function(answer){
-                    return (Number.isNaN(answer)) ? 'Please enter a valid number' : true;
-                }
-            },
-            // Prompt user for new product stock if relevant
-            {
-                type: 'number',
-                name: 'newProductStock',
-                message: 'Stock of product to add?',
-                when: function(answers){
-                    return answers.newProductPrice !== undefined;
-                },
-                default: 1,
-                validate: function(answer){
-                    return (Number.isNaN(answer)) ? 'Please enter a valid number' : true;
-                }
+            default: 1,
+            validate: function(answer){
+                return (Number.isNaN(answer)) ? 'Please enter a valid number' : true;
             }
-        ]).then(function(answers){
-            console.log(answers);
-            connection.end();
-        });
+        },
+        // Prompt user for new product name if Add new product chosen
+        {
+            type: 'input',
+            name: 'newProductName',
+            message: 'Name of product to add?',
+            when: function(answers) {
+                return answers.chooseAction === 'Add new product';
+            },
+        },
+        // Prompt user for new product department if relevant
+        {
+            type: 'input',
+            name: 'newProductDepartment',
+            message: 'Department of product to add?',
+            when: function(answers) {
+                return answers.newProductName !== undefined;
+            }
+        },
+        // Prompt user for new product price if relevant
+        {
+            type: 'number',
+            name: 'newProductPrice',
+            message: 'Price of product to add?',
+            when: function(answers){
+                return answers.newProductDepartment !== undefined;
+            },
+            default: 1.00,
+            validate: function(answer){
+                return (Number.isNaN(answer)) ? 'Please enter a valid number' : true;
+            }
+        },
+        // Prompt user for new product stock if relevant
+        {
+            type: 'number',
+            name: 'newProductStock',
+            message: 'Stock of product to add?',
+            when: function(answers){
+                return answers.newProductPrice !== undefined;
+            },
+            default: 1,
+            validate: function(answer){
+                return (Number.isNaN(answer)) ? 'Please enter a valid number' : true;
+            }
+        }
+    ]).then(function(answers){
+        console.log(answers);
+        connection.end();
     });
 };
-managerInput();
+async function populateInventory(flag) {
+    const queryURL = `SELECT item_id, product_name, price, stock_quantity FROM products ${(flag === 'lowStock') ? 'WHERE stock_quantity <= 5' : ''}`
+    const output = (await mysqlDB.query(queryURL))[0];
+    console.log(output);
+    if (flag === 'addStock') {
+        return output.map(product => `${product.item_id}) ${product.product_name} Stock: ${product.stock_quantity}`);
+    }
+    else {
+        return output;
+    };
+};
